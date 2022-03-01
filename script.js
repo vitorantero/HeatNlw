05a37fd2c1f1f9b83e837178264eb5a2d9a16f54
Vitor Antero <88521707+vitorantero@users.noreply.github.com>
2022-03-01 20:42:33 -0300

    Fist Commit HeatNlw
    Projeto realizado com base na NLW Heat da Rocketseat, colocado em pratico conhecimentos obtidos com o HTML, CSS e JavaScript

---
+++ script.js
@@ -0,0 +1,33 @@
+const LinkSocialMedia = {
+  github: 'vitorantero',
+  youtube: 'vitorantero',
+  facebook: 'Vitor Antero',
+  instagram: '___vitorantero',
+  twitter: 'vitorantero'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinkSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinkSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImg.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
